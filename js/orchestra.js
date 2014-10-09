067a106c2d230ee2cf2947f9d6276558d48c52e5
Noah Laferty <noah.lafferty@gmail.com>
2014-10-09 02:26:50 -0500

    arrow orientation

--- js/orchestra.js
+++ js/orchestra.js
@@ -75,10 +75,10 @@ $(document).ready(function() {
 
                     if (p.left+w/2 < window.innerWidth/2) {
                         $("#arrow").css("left", (p.left+w-40) + "px");
-                        $("#arrow").addClass("flipH");
+                        $("#arrow").removeClass("flipH");
                     } else {
                         $("#arrow").css("left", (p.left-$("#arrow").width()+40) + "px");
-                        $("#arrow").removeClass("flipH");
+                        $("#arrow").addClass("flipH");
                     }
 
                     if (p.top+h/2 < window.innerHeight/2) {
