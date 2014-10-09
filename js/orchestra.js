7ab6b2210958413f9761c69126534ce4a01ed231
Noah Laferty <noah.lafferty@gmail.com>
2014-10-09 04:56:31 -0500

    positioning

--- js/orchestra.js
+++ js/orchestra.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
 
                     if (p.top+h/2 < window.innerHeight/2) {
                         $("#arrow").css("top", (p.top+h-40) + "px");
-                        $("#detail").css("top", (p.top+h+60) + "px");
+                        $("#detail").css("top", (p.top+h) + "px");
                         flip += 2;
                     }
                     else {
